951742de5f6b75266ce94f54505c82bb3a449b3e
Ritik Nandanwar <nandanwarritik10@gmail.com>
2024-05-06 16:54:58 +0530

    Broadcast logic done!

---
+++ chat app/client/src/App.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+function App() {
+  const socket = useMemo(() => io("http://localhost:8080"), []);
+  const [message, setMessage] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+  const [roomId, setRoomId] = useState("");
+  const [socketId, setSocketId] = useState("");
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to ", socket.id);
+      setSocketId(socket.id);
+    });
+    socket.on("send-message", (data) => {
+      setAllMessages((allMessages) => [...allMessages, data]);
+    });
+    socket.on("send-message", ({ data }) => {
+      console.log(data);
+    });
+    socket.on("user-joined-room", (data) => {
+      console.log(data);
+    });
+    return () => socket.disconnect();
+  }, []);
+  const clickHandler = (e) => {
+    e.preventDefault();
+    socket.emit("clickclack", "ciiieie");
+    socket.emit("receive-message", message);
+  };
+  const handleChange = (e) => {
+    console.log(message);
+    setMessage(e.target.value);
+  };
+  const handleRoomIdChange = (e) => {
+    console.log("roomId ", roomId);
+    setRoomId(e.target.value);
+    socket.emit("join-room", roomId);
+  };
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", { socketId, roomId });
+  };
+  return (
+    <>
+      <div className="container" style={{ maxWidth: "720px" }}>
+        {allMessages.map((msg) => (
+          <h4 key={msg} className="card p-2">
+            {msg}
+          </h4>
+        ))}
+      </div>
+      <form
+        className="container"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          maxWidth: "720px",
+        }}
+      >
+        <hr />
+        <input
+          type="text"
+          value={message}
+          className="grey lighten-4 "
+          onChange={handleChange}
+        />
+        <button className="btn green" onClick={clickHandler}>
+          Send
+        </button>
+      </form>
+      <form
+        className="container"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          maxWidth: "720px",
+        }}
+      >
+        <hr />
+        <input
+          type="text"
+          value={roomId}
+          className="grey lighten-4"
+          onChange={handleRoomIdChange}
+          placeholder="Enter room ID"
+        />
+        <button className="btn green" onClick={joinRoomHandler}>
+          Join_Room
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ chat app/server/index.js
@@ -0,0 +1,44 @@
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const server = require("http").createServer(app);
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Initiate socket connection
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    METHODS: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("new conn ,", socket.id);
+  socket.on("clickclack", (p) => {
+    console.log(p);
+  });
+  socket.on("receive-message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("send-message", data);
+  });
+  socket.on("join-room", ({ socketId, roomId }) => {
+    socket.join(roomId);
+    socket
+      .to(roomId)
+      .emit("user-joined-room", `a new user joined the room ${socketId}`);
+  });
+});
+
+//Routes
+app.get("/", (req, res) => {
+  res.send({ message: "Hello world" });
+});
+//Listening on port 8080
+server.listen(8080, () => {
+  console.log("LISTENING");
+});
